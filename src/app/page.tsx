8085a0762f0e99d79ab6a0d348cf3decab26f6d6
Vladislav <vgrechannik@gmail.com>
2024-04-30 07:05:35 +0200

    Make it load faster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
-
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
@@ -38,28 +36,40 @@ export default async function Home() {
             </div>
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
 
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
-        </div>
+        <Suspense fallback={<div>Loading info...</div>}>
+          <LoginInfo />
+        </Suspense>
 
-        <CrudShowcase />
+        <Suspense fallback={<div>Loading Post UI...</div>}>
+          <CrudShowcase />
+        </Suspense>
       </div>
     </main>
   );
+}
+
+async function LoginInfo() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  return <div className="flex flex-col items-center gap-2">
+    <p className="text-2xl text-white">
+      {hello.greeting}
+    </p>
+
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {session && <span>Logged in as {session.user?.name}</span>}
+      </p>
+      <Link
+        href={session ? "/api/auth/signout" : "/api/auth/signin"}
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+      >
+        {session ? "Sign out" : "Sign in"}
+      </Link>
+    </div>
+  </div>
 }
 
 async function CrudShowcase() {
